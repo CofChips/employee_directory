ba3e721d91436ace6e328338d17520bea6a8fff2
CofChips <qiwei.mod@gmail.com>
2020-08-12 16:27:55 -0700

    added props for onchange in search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
 
-function Search(){
+function Search(props){
 
     return(
         <div className="search m-auto pt-5">
-        <input type="search" className="form-control" id="search" placeholder="Search"/>
+        <input type="search" className="form-control" id="search" placeholder="Search" onChange={props.onChange}/>
         {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
       </div>
     )
