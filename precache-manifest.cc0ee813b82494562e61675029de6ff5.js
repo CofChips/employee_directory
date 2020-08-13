5ccdc44e668a220dc8b390b737dd4b6db57aacf3
CofChips <qiwei.mod@gmail.com>
2020-08-12 22:02:02 -0700

    Updates

---
+++ precache-manifest.cc0ee813b82494562e61675029de6ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a51edb49da7ca73b1a5f5b33484dc40",
+    "url": "/employee_directory/index.html"
+  },
+  {
+    "revision": "bc5e01ec37651a9dde3b",
+    "url": "/employee_directory/static/css/main.64f3c2fe.chunk.css"
+  },
+  {
+    "revision": "cf81fc23ceca2c23e821",
+    "url": "/employee_directory/static/js/2.be73fbea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee_directory/static/js/2.be73fbea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5e01ec37651a9dde3b",
+    "url": "/employee_directory/static/js/main.a2ad6f68.chunk.js"
+  },
+  {
+    "revision": "498fd09be4c9f38769b0",
+    "url": "/employee_directory/static/js/runtime-main.792f3a3a.js"
+  }
+]);
